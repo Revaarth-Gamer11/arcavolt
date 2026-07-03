b72bc94282c44688e7fab7ba57cbe6eea94c6125
Revaarth Sabharwal <Revaarth@Rahuls-MacBook-Air.local>
2026-07-03 20:51:48 +1000

    fix openGame routing

--- main.js
+++ main.js
@@ -1,18 +1,92 @@
-function openGame(id){
 
-    const app = document.getElementById("app");
+function openGame(i){
 
-    if(id === 0){
-        startTicTacToe();
-    }
-    else if(id === 1){
-        startRPS();
-    }
-    else{
-        app.innerHTML = `
-            <h2>${games[id]}</h2>
-            <p>🚧 Coming next update</p>
-            <button onclick="showMenu()">⬅ Back</button>
-        `;
+    const game = games[i];
+    const name = game.name;
+
+    switch(name){
+
+        case "Tic Tac Toe":
+            startTicTacToe();
+            break;
+
+        case "Rock Paper Scissors":
+            startRPS();
+            break;
+
+        case "Snake":
+            startSnake();
+            break;
+
+        case "Memory":
+            startMemory();
+            break;
+
+        case "Reaction":
+            startReaction();
+            break;
+
+        case "Click Speed":
+            startClick();
+            break;
+
+        case "Whack A Mole":
+            startWhackAMole();
+            break;
+
+        case "Connect Four":
+            startConnectFour();
+            break;
+
+        case "Pong":
+            startPong();
+            break;
+
+        case "Breakout":
+            startBreakout();
+            break;
+
+        case "2048":
+            start2048();
+            break;
+
+        case "Minesweeper":
+            startMinesweeper();
+            break;
+
+        case "Simon Says":
+            startSimonSays();
+            break;
+
+        case "Hangman":
+            startHangman();
+            break;
+
+        case "Dots & Boxes":
+            startDotsBoxes();
+            break;
+
+        case "Space Shooter":
+            startSpaceShooter();
+            break;
+
+        case "Flappy Bird":
+            startFlappyBird();
+            break;
+
+        case "Platform Runner":
+            startPlatformRunner();
+            break;
+
+        case "Maze Escape":
+            startMazeEscape();
+            break;
+
+        case "Colour Match":
+            startColourMatch();
+            break;
+
+        default:
+            alert("🚧 Game not ready yet!");
     }
 }
