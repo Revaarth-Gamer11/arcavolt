8f73d1bb8a510dc5ae1807b7e1bc68d46f544c44
Revaarth Sabharwal <revaarth.sabharwal@gmail.com>
2026-07-06 18:42:10 +1000

    fix menu auto load

--- main.js
+++ main.js
@@ -1,92 +1,20 @@
+window.addEventListener("load", function(){
 
-function openGame(i){
+    let wins = localStorage.getItem("wins") || 0;
 
-    const game = games[i];
-    const name = game.name;
-
-    switch(name){
-
-        case "Tic Tac Toe":
-            startTicTacToe();
-            break;
-
-        case "Rock Paper Scissors":
-            startRPS();
-            break;
-
-        case "Snake":
-            startSnake();
-            break;
-
-        case "Memory":
-            startMemory();
-            break;
-
-        case "Reaction":
-            startReaction();
-            break;
-
-        case "Click Speed":
-            startClick();
-            break;
-
-        case "Whack A Mole":
-            startWhackAMole();
-            break;
-
-        case "Connect Four":
-            startConnectFour();
-            break;
-
-        case "Pong":
-            startPong();
-            break;
-
-        case "Breakout":
-            startBreakout();
-            break;
-
-        case "2048":
-            start2048();
-            break;
-
-        case "Minesweeper":
-            startMinesweeper();
-            break;
-
-        case "Simon Says":
-            startSimonSays();
-            break;
-
-        case "Hangman":
-            startHangman();
-            break;
-
-        case "Dots & Boxes":
-            startDotsBoxes();
-            break;
-
-        case "Space Shooter":
-            startSpaceShooter();
-            break;
-
-        case "Flappy Bird":
-            startFlappyBird();
-            break;
-
-        case "Platform Runner":
-            startPlatformRunner();
-            break;
-
-        case "Maze Escape":
-            startMazeEscape();
-            break;
-
-        case "Colour Match":
-            startColourMatch();
-            break;
-
-        default:
-            alert("🚧 Game not ready yet!");
+    const winEl = document.getElementById("wins");
+    if(winEl){
+        winEl.innerText = wins;
     }
-}
+
+    // HARD CHECK (wait until everything is ready)
+    function tryStart(){
+        if(typeof showMenu === "function"){
+            showMenu();
+        } else {
+            setTimeout(tryStart, 50);
+        }
+    }
+
+    tryStart();
+});
